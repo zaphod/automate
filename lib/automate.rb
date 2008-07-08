234a693c9de67a4dd98995e0b24705f3343fdf5e
Sudhindra Rao <srao>
2008-07-08 11:36:33 +0530

    Sudhindra : added gem

--- lib/automate.rb
+++ lib/automate.rb
@@ -3,12 +3,8 @@ class Automate
   def self.run
     unless($*.empty?)
       project = "#{Dir.pwd}/#{$*}"
-      puts "Automating project #{project}"
-      tasks = "#{project}/lib/tasks"
-      puts "Copying rake tasks to #{tasks}"
-      gemdir = File.expand_path(File.dirname(__FILE__))
-      command = "cp -R #{gemdir}/* '#{tasks}'"
-      system command
+      target = mkdir(project)
+      target = copy_rake_tasks(target)
     else
       usage
     end
@@ -18,4 +14,20 @@ class Automate
     puts "Usage : automate <project_name>"
   end
   
+  def self.mkdir(project)
+    puts "Automating project #{project}"
+    target = "#{project}/lib/tasks/automate"
+    puts "Making directory #{target}"
+    command = "mkdir '#{target}'"
+    system command
+    target    
+  end
+
+  def self.copy_rake_tasks(target)
+    puts "Copying rake tasks to #{target}"
+    gemdir = File.expand_path(File.dirname(__FILE__))
+    command = "cp -R #{gemdir}/* '#{target}'"
+    system command
+    target    
+  end
 end
